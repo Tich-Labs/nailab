eabc3beb45c0c13795c9c22564fc52ca97e146ab
nashthecoder <naijeria@gmail.com>
2025-12-19 01:46:49 +0300

    Add modal mentorship request form to founder dashboard

--- app/controllers/founder/mentorship_controller.rb
+++ app/controllers/founder/mentorship_controller.rb
@@ -4,5 +4,11 @@ class Founder::MentorshipController < Founder::BaseController
     @mentors = Mentor.all
     @mentorship_requests = current_user.mentorship_requests
     @sessions = current_user.sessions
+    
+    # Handle mentorship request modal
+    if params[:request_mentor].present?
+      @selected_mentor = Mentor.find_by(id: params[:request_mentor])
+      @mentorship_request = current_user.mentorship_requests.build(mentor: @selected_mentor)
+    end
   end
 end
