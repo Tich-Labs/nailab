2b2165716bb518f7c5ff6de13d9ee4bc0c84f198
nashthecoder <naijeria@gmail.com>
2026-01-08 19:14:57 +0300

    fix: add app/javascript to asset paths and precompile list for importmap

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,10 @@ Rails.application.configure do
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # Precompile JavaScript files from app/javascript for importmap
+  config.assets.paths << Rails.root.join("app/javascript")
+  config.assets.precompile += %w[ application.js admin.js ]
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
