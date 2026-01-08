d6a21b83aa9fd0fb4238fde34c81db66e3db0bbf
nashthecoder <naijeria@gmail.com>
2026-01-08 14:58:15 +0300

    Fix deployment issues: remove obsolete controller and puma_worker_killer dependency

--- config/puma.rb
+++ config/puma.rb
@@ -45,15 +45,4 @@ pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
 if ENV.fetch("RAILS_ENV", "development") == "production"
   workers ENV.fetch("WEB_CONCURRENCY", 2)
   preload_app!
-  
-  before_fork do
-    require 'puma_worker_killer'
-    PumaWorkerKiller.config do |config|
-      config.ram = 1024 # MB
-      config.frequency = 5 # seconds
-      config.percent_usage = 0.98
-      config.rolling_restart_frequency = 6 * 3600 # 6 hours
-    end
-    PumaWorkerKiller.start
-  end
 end
