49b4de5a31d7d0b3b4ef16bd67de2e7d517a9765
nashthecoder <naijeria@gmail.com>
2026-01-09 22:23:28 +0300

    ProgramsPage: use ActionText :intro and add program_sections association

--- app/models/programs_page.rb
+++ app/models/programs_page.rb
@@ -1,2 +1,6 @@
 class ProgramsPage < ApplicationRecord
+  has_rich_text :intro
+
+  has_many :program_sections, -> { order(:position) }, dependent: :destroy
+  accepts_nested_attributes_for :program_sections, allow_destroy: true, reject_if: :all_blank
 end
